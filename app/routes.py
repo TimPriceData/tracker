b918f289949b3d1c29c45bd879d140c3bf2d2b6f
Timothy Price <blank@blank.com>
2019-12-31 09:49:35 +1030

    Added templates and dummy data

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,18 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Hello world!'
+    users = {'username': 'Tim'}
+    logs = [
+        {
+            'owner': {'username': 'Tim'},
+            'record': '30 minutes shallow work'
+        },
+        {
+            'owner': {'username': 'Tim'},
+            'record': '60 minutes concentrated work'
+        }
+    ]
+    return render_template('index.html', title='home', users=users, logs=logs)
