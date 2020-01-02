954fd2a561da710663d2c55e54730557b0b19853
Timothy Price <blank@blank.com>
2020-01-02 18:06:03 +1030

    Added login functionality

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,13 @@
 from flask import render_template, flash, url_for, redirect
 from app import app
 from app.forms import LoginForm
+from flask_login import current_user, login_user, logout_user
+from flask_login import login_required
+from app.models import User
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
     users = {'username': 'Tim'}
     logs = [
@@ -16,14 +20,25 @@ def index():
             'record': '60 minutes concentrated work'
         }
     ]
-    return render_template('index.html', title='home', users=users, logs=logs)
+    return render_template('index.html', title='home', logs=logs)
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember me={}'.format(
-            form.username.data, form.remember_me.data))
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
         return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
